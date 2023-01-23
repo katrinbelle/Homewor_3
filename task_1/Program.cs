fd909c44887e870163f3fb59d065e2989c72c54e
Ekaterina <kat.bommon@gmail.com>
2023-01-23 07:44:16 +0300

    program for finding polydrome number

---
+++ task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да */
+
+int GetNumber (string message)
+{
+    int result = 0;
+    Console.Write(message);
+    while(true)
+{
+    if (int.TryParse(Console.ReadLine(), out result)&& result < 100000 && result > 9999) break;
+    else Console.WriteLine("Введите пятизначное число, ранее ввели не то -> ");
+}
+return result;
+}
+ int number = GetNumber("Введите пятизначное число-> ");
+
+    if (number/10000==number%10 && number/1000%10== number%100/10  ) Console.Write(" Число -> "+number+" является палиндромом");
+ else Console.Write(" Число -> "+number+" не является палиндромом");
